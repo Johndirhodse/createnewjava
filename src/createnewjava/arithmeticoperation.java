0f84828c427ccf01470fdd5c91c3008b6f3f7004
johndirhodse <johndirhodes@gmail.com>
2022-08-30 08:24:14 +0530

    swapping A&B

---
+++ src/createnewjava/arithmeticoperation.java
@@ -0,0 +1,19 @@
+package createnewjava;
+
+public class arithmeticoperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=6;
+		a=a+b; //10+6=16
+		b=a-b; //16-6=10
+		a=a-b; //16-10=6
+		System.out.println("After swapping values are..  "+a+" "+b);
+	
+	
+	
+	
+	}
+
+}
---
+++ src/createnewjava/module.java
@@ -0,0 +1,19 @@
+package createnewjava;
+
+public class module {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=320;
+		int a,b,c,d;
+		a=num/10/;
+		b=num%10;
+		c=a/10;
+		d=a%10;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(b);
+
+	}
+
+}
