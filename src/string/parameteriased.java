e60c71d1e90a83c253e88a5fd4ffd6021cfee8ed
johndirhodse <johndirhodes@gmail.com>
2022-09-30 18:17:37 +0530

    using parameters

---
+++ src/string/parameteriased.java
@@ -0,0 +1,14 @@
+package string;
+
+public class parameteriased {
+	void print(int a) {
+	System.out.println(a);
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    parameteriased p=new parameteriased();
+     p.print(5);
+	}
+
+}
