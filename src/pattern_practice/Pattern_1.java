b5043f9e38398c2a07cea65feb683d44757dc9e8
johndirhodse <johndirhodes@gmail.com>
2022-10-03 20:02:20 +0530

    pattern program *

--- src/pattern_practice/Pattern_1.java
+++ src/pattern_practice/Pattern_1.java
@@ -4,14 +4,13 @@ public class Pattern_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=10;
-for (int i=1;i<n;i++) {
-	for (int j=i;j<n;j++) {
-		System.out.print("* ");
-	}
-	System.out.println();
-}
-	
+		for(int i=0;i<10;i++) {
+			for(int j=i;j<10;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
