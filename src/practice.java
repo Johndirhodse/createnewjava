e9cc3c260881e22c9b8df48835b99c305d80c5b5
johndirhodse <johndirhodes@gmail.com>
2022-11-05 21:54:38 +0530

    find the divisor in two values

---
+++ src/practice.java
@@ -0,0 +1,19 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1 =100;
+int num2 =120;
+int gcd=0;
+int div =2;
+int small=num1<num2?num1:num2;
+while(div<=small) {
+	if(num1%div==0&&num1%div==0) 
+		gcd=div;
+	div =div+1;
+}
+System.out.print(gcd);
+	}
+
+}
