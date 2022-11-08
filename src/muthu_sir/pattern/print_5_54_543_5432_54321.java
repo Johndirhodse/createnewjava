ca0c8d12e2050ae57aa02959796a2432394d9e81
johndirhodse <johndirhodes@gmail.com>
2022-11-08 13:07:28 +0530

    pattern_5_54_543_5432

---
+++ src/muthu_sir/pattern/print_5_54_543_5432_54321.java
@@ -0,0 +1,15 @@
+package muthu_sir.pattern;
+
+public class print_5_54_543_5432_54321 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int row=5;row>=1;row--) {
+	for(int col=5;col>=row;col--) {
+		System.out.print(col);
+	}
+	System.out.println();
+}
+	}
+
+}
